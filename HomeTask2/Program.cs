4fe7ed2e50c9d3ba7b62ce9d6b5a5c29f99f6979
Yuliya <i@yzubkova.ru>
2023-01-21 21:44:26 +0300

    ДЗ 2 - программа отображает третью цифру вводимого числа

--- HomeTask2/Program.cs
+++ HomeTask2/Program.cs
@@ -10,4 +10,33 @@ string number1 = Console.ReadLine();
 int number = Convert.ToInt32(number1);
 int count = number.ToString().Length;
 
+// ФУНКЦИИ------------------------------------------------------------------------------------------------------
 
+// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем.
+
+Console.Write(MakeArray(number, count));
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+// Функция принимает число введенное пользователем, количество символов, и выводит третью цифру числа. Если 3 цифры нет, сообщает и выводит 0.
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет, держи: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
