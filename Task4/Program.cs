41e6d2de75f1db7b98ed1ac0a08b3f12ff2090bf
Yuliya <i@yzubkova.ru>
2023-01-21 19:17:54 +0300

    Задача на проверку кратности введенного числа двум числам

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+● 14 -> нет
+● 46 -> нет
+● 161 -> да */
+
+Console.WriteLine("Введите число: ");
+string number1 = Console.ReadLine();
+int numberOne = Convert.ToInt32(number1);
+
+Console.Write(numberOne+" -> ");
+
+if (numberOne % (7*23) == 0)
+    Console.WriteLine("да");
+else Console.WriteLine("нет");
+
