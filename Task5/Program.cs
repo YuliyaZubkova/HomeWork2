bee3756c1dfe1ecc5ba4dd00a253a81f938d6a12
Yuliya <i@yzubkova.ru>
2023-01-21 22:25:45 +0300

    Программа проверяет является ли одно число квадратом другого

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -3,4 +3,17 @@
 
 Console.WriteLine("Введите первое число: ");
 string number1 = Console.ReadLine();
-Console.Write(MakeArray(number, count));
+int numberOne = Convert.ToInt32(number1);
+
+Console.WriteLine("Введите второе число: ");
+string number2 = Console.ReadLine();
+int numberTwo = Convert.ToInt32(number2);
+
+if(numberOne * numberOne == numberTwo || numberTwo * numberTwo == numberOne)
+    {
+        Console.WriteLine("Да");
+        }
+else
+{
+    Console.WriteLine("Нет");
+}
