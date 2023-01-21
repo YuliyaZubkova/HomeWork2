19036bc10f0a901999f893af20561147df06843a
Yuliya <i@yzubkova.ru>
2023-01-21 12:18:02 +0300

    Задача показать большее из двух случайных чисел

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+Random rnd = new Random();
+int number = rnd.Next(10, 100); //[10, 100]
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if(firstDigit > secondDigit)
+        Console.WriteLine($"Число {firstDigit} больше чем {secondDigit}");
+else if (secondDigit > firstDigit)
+        Console.WriteLine($"Число {secondDigit} больше чем {firstDigit}");
+else 
+        Console.WriteLine("Числа равны");
