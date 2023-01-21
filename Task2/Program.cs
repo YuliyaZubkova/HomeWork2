09c8d40d2eb52a44b0e09f4f8872d0b8543d716e
Yuliya <i@yzubkova.ru>
2023-01-21 12:36:15 +0300

    Задача выводит случайное 3значное число и удаляет его вторую цифру

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+Random rnd = new Random();
+int number = rnd.Next(100, 1000); //[100, 1000]
+int firstDigit = number / 100;
+int secondDigit = number % 10;
+
+Console.WriteLine(firstDigit, secondDigit);
+
+
+/* if(firstDigit > secondDigit)
+        Console.WriteLine($"Число {firstDigit} больше чем {secondDigit}");
+else if (secondDigit > firstDigit)
+        Console.WriteLine($"Число {secondDigit} больше чем {firstDigit}");
+else 
+        Console.WriteLine("Числа равны");
