5a93d52ca7c8be801db3af38eeea3d791b9c26d2
Yuliya <i@yzubkova.ru>
2023-01-30 13:19:47 +0300

    Программа выводит вторую цифру заданного трехзначного числа

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -1,24 +1,33 @@
 ﻿/* Задача 10: Напишите программу, 
-которая принимает на вход трёхзначное число 
-и на выходе показывает вторую цифру этого числа. 
-456 -> 5 
+которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
 782 -> 8
-453 -> 5
+918 -> 1
 */
 
-Console.WriteLine("Введите трехзначное число: ");
-string number1 = Console.ReadLine();
-int number = Convert.ToInt32(number1);
+//Console.WriteLine("Введите трехзначное число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
 
-Console.Write(number);
-Console.Write(" -> ");
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
 
-if (number < 1000 && number >100)
+void SecondNumber(int numberOne)
+{
+if (numberOne < 1000 && numberOne >100)
     {
-    int firstDigit = number % 100;
+    int firstDigit = numberOne % 100;
     int secondDigit = firstDigit/10;
-    Console.Write(secondDigit);
+    Console.Write($"Вторая цифра числа {numberOne} -> {secondDigit} ");
 }
 else  Console.Write("Введенное число не трехзначное - введите трехзначное число.");
+}
 
+int numberOne = GetNumber("Введите трехзначное число: ");
+SecondNumber(numberOne);
 
