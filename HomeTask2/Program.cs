8d3f44c64301f68419a3401af263a85fca4a4318
Yuliya <i@yzubkova.ru>
2023-01-30 13:40:53 +0300

    ДЗ 2: Выводим третью цифру заданого числа

--- HomeTask2/Program.cs
+++ HomeTask2/Program.cs
@@ -5,28 +5,12 @@
 32679 -> 6
 */
 
-Console.WriteLine("Введите число: ");
-string number1 = Console.ReadLine();
-int number = Convert.ToInt32(number1);
-int count = number.ToString().Length;
-
-// ФУНКЦИИ------------------------------------------------------------------------------------------------------
-
-// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем.
-
-Console.Write(MakeArray(number, count));
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
-// Функция принимает число введенное пользователем, количество символов, и выводит третью цифру числа. Если 3 цифры нет, сообщает и выводит 0.
 int MakeArray(int a, int b)
 {
-int result = 0;
+    int result = 0;
     if (b < 3)
     {
-        Console.Write("Третьей цифры нет, держи: ");
+        Console.Write("Третьей цифры нет");
     }
     else
     {
@@ -38,5 +22,11 @@ int result = 0;
 
         result = (a / c) % 10;
     }
-return result;
+    return result;
 }
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number.ToString().Length;
+
+Console.Write(MakeArray(number, count));
